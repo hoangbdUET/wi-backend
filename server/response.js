4fa98ecb3f6bb24cff0432da83d633754cc4ccc5
tanlm <tanlm@54.202.4.43>
2017-07-04 12:03:26 +0700

    add response.js

---
+++ server/response.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class Response {
+    constructor(code,reason,content) {
+        this.code = code;
+        this.reason = reason;
+        this.content = content;
+    }
+}
+
+/**
+ * Response success format.
+ *
+ * @param code
+ * @param reason
+ * @param content
+ * @returns {Response}
+ */
+module.exports = function response(code,reason,content) {
+    code = code || 200;
+    reason=reason || "";
+    content = content || {};
+    return new Response(code,reason,content);
+};
