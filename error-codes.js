f21032df0bb094455316b36fc3e87b918072cab5
minhnt58 <tienminh.uet@gmail.com>
2017-07-18 14:05:57 +0700

    fixed error in data curve file

--- error-codes.js
+++ error-codes.js
@@ -3,6 +3,7 @@ module.exports.CODES = {
     ERROR_FIELD_EMPTY: 510,
     ERROR_ENTITY_NOT_EXISTS: 511,
     ERROR_INVALID_PARAMS: 512,//wrong input
+    ERROR_CURVE_DATA_FILE_NOT_EXISTS: 513,
     ERROR_SYNC_TABLE: 520,//Common reason is connect to database fail.
     ERROR_DELETE_DENIED: 521//Error can't delete. common reason is foreignKey Constraint
 };
