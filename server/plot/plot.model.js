2f2d1bd41500cb6e3750512a8c91d6d9daa71b64
root <root@ip-172-31-27-226.ap-southeast-1.compute.internal>
2017-07-24 10:53:20 +0000

    update

--- server/plot/plot.model.js
+++ server/plot/plot.model.js
@@ -15,7 +15,7 @@ function createNewPlot(plotInfo, done) {
                 });
                 plot.save()
                     .then(function (plot) {
-                        done(ResponseJSON(ErrorCodes.SUCCESS,"Create new Plot success",{idPlot:plot.idPlot}));
+                        done(ResponseJSON(ErrorCodes.SUCCESS,"Create new Plot success",plot.toJSON()));
                     })
                     .catch(function (err) {
                         done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Create new Plot "+err.name));
