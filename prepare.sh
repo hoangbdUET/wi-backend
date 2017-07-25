3c31e370756c83c93cc12e995a6038b9beaabbeb
luuminhtan046@gmail.com <luuminhtan046@gmail.com>
2017-07-25 10:49:42 +0700

    fix prepare

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mysql -u root -pqwertyui -e "DROP DATABASE IF EXISTS wi_backend;"
-mysql -u root -pqwertyui -e "CREATE DATABASE wi_backend;"
+mysql -u root -pqwertyui -e "CREATE DATABASE IF NOT EXISTS wi_backend;"
