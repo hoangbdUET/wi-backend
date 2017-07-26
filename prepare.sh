798c80bae022fc85db8f218fdf5efd2230a34c9d
luuminhtan046@gmail.com <luuminhtan046@gmail.com>
2017-07-26 17:30:58 +0700

    can not create new with id of parent is null

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 mysql -u root -pqwertyui -e "DROP DATABASE IF EXISTS wi_backend;"
 mysql -u root -pqwertyui -e "CREATE DATABASE IF NOT EXISTS wi_backend;"
+mkdir uploads
+mkdir uploads_temp
+mkdir ../data
