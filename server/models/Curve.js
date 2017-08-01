8e635e95f4d562c742c7eaefeeedd81c2d3f0940
luuminhtan046@gmail.com <luuminhtan046@gmail.com>
2017-08-01 14:55:20 +0700

    Revert "Revert "Edit Line, add unique to Curve,Well,Dataset""
    This reverts commit f8beac3ffbc4adfcb4036c09d118cff14624dfed.

--- server/models/Curve.js
+++ server/models/Curve.js
@@ -7,7 +7,8 @@ module.exports = function (sequelize, DataTypes) {
         },
         name:{
             type:DataTypes.STRING(50),
-            allowNull:false
+            allowNull:false,
+            unique:"name-idDataset"
         },
         unit:{
             type:DataTypes.STRING(250),
