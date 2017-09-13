1e52407833a1341e6a3aabba682cba21b4bacffe
hoangbdUET <bdhoang.ht@gmail.com>
2017-09-13 10:41:21 +0700

    update clear custom-fill

--- server/custom-fill/index.js
+++ server/custom-fill/index.js
@@ -13,8 +13,7 @@ let bodyParser = require('body-parser');
 router.use(cors());
 
 router.post('/save', (req, res) => {
-    //appendFileSync
-    let command = "";
+    //appendFileSync if exists
     let newFill = new Object();
     let exists = false;
     newFill.name = req.body.name;
@@ -26,17 +25,25 @@ router.post('/save', (req, res) => {
             exists = true;
         }
     });
-    if (exists) {
-        fs.writeFileSync(dataFile, JSON.stringify(oldData));
-    } else {
+    if (!exists) {
         oldData.push(newFill);
         fs.writeFileSync(dataFile, JSON.stringify(oldData));
+        res.send(ResponseJSON(errorCodes.CODES.SUCCESS, "Added new custom-fill", newFill));
+    } else {
+        fs.writeFileSync(dataFile, JSON.stringify(oldData));
+        res.send(ResponseJSON(errorCodes.CODES.SUCCESS, "Updated custom-fill", newFill));
     }
-    res.send(ResponseJSON(errorCodes.CODES.SUCCESS, command, newFill));
 });
 
 router.post('/all', (req, res) => {
     let data = require('./data.json');
     res.send(ResponseJSON(errorCodes.CODES.SUCCESS, "Successful", data));
 });
+
+router.post('/clear', (req, res) => {
+    let data = new Array();
+    fs.writeFileSync(dataFile, JSON.stringify(data));
+    res.send(ResponseJSON(errorCodes.CODES.SUCCESS, "Clear data successfull"));
+});
+
 module.exports = router;
