7c0529694d0122d02ae27861d77263a4c0e50acd
hoangbdUET <bdhoang.ht@gmail.com>
2017-09-21 07:04:54 +0000

    update prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 mysql -u root -pqwertyui -e "DROP DATABASE IF EXISTS wi_backend;"
+DBS="$(mysql -u root -pqwertyui -Bse "show databases like 'wi_%'")"
+for db in $DBS; do
+echo "===================>>>>>>>>>>>Deleting $db"
+mysql -u root -pqwertyui -Bse "drop database $db"
+done
 mysql -u root -pqwertyui -e "CREATE DATABASE IF NOT EXISTS wi_backend;"
+
