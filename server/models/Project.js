0b276783f8a3001300cd5a6c5d2ba10a733e9ea3
hoangbdUET <bdhoang.ht@gmail.com>
2017-10-10 18:53:55 +0700

    Unique project name

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,32 +1,33 @@
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('project',{
-        idProject:{
-            type:DataTypes.INTEGER,
+    return sequelize.define('project', {
+        idProject: {
+            type: DataTypes.INTEGER,
             autoIncrement: true,
-            primaryKey:true
+            primaryKey: true
         },
-        name:{
-            type:DataTypes.STRING(50),
-            allowNull:false,
-            validate:{
-                notEmpty:true
+        name: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true
             }
         },
-        location:{
-            type:DataTypes.STRING(250),
-            allowNull:true
+        location: {
+            type: DataTypes.STRING(250),
+            allowNull: true
         },
-        company:{
-            type:DataTypes.STRING(250),
-            allowNull:true
+        company: {
+            type: DataTypes.STRING(250),
+            allowNull: true
         },
-        department:{
-            type:DataTypes.STRING(250),
-            allowNull:true
+        department: {
+            type: DataTypes.STRING(250),
+            allowNull: true
         },
-        description:{
-            type:DataTypes.STRING(250),
-            allowNull:true
+        description: {
+            type: DataTypes.STRING(250),
+            allowNull: true
         }
     });
 };
