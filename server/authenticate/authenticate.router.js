77a9b166adde6467b1d57d884c220e639e1781c4
hoangbdUET <bdhoang.ht@gmail.com>
2017-10-30 14:55:22 +0700

    fix capcha

--- server/authenticate/authenticate.router.js
+++ server/authenticate/authenticate.router.js
@@ -120,7 +120,7 @@ router.post('/register', function (req, res) {
                 res.status(401).send(ResponseJSON(ErrorCodes.ERROR_USER_EXISTED, "User existed!"));
             })
     } else {
-        captchaList.delete(req.body.captcha);
+        // captchaList.delete(req.body.captcha);
         res.status(200).send(ResponseJSON(ErrorCodes.SUCCESS, "Captcha is not correct!", "CAPTCHA"));
     }
 });
