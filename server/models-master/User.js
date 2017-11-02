d49657ce093904a6c21b44b5e696e884ac8cb61b
hoangbdUET <bdhoang.ht@gmail.com>
2017-11-02 15:54:22 +0700

    update

--- server/models-master/User.js
+++ server/models-master/User.js
@@ -19,7 +19,7 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING(100),
             allowNull: true,
             // defaultValue: "Inactive"
-            defaultValue: "Inactive"
+            defaultValue: "Actived"
         },
         /*
         1 : Administrator
