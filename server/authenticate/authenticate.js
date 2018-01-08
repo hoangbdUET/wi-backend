b4526d5e90230dd822137e98f91556f0151f1076
Hoang Bui <bdhoang.ht@gmail.com>
2018-01-08 15:50:15 +0700

    fix

--- server/authenticate/authenticate.js
+++ server/authenticate/authenticate.js
@@ -1,26 +1,26 @@
-var jwt = require('jsonwebtoken');
-var models = require('../models');
-var ErrorCodes = require('../../error-codes').CODES;
-var ResponseJSON = require('../response');
+let jwt = require('jsonwebtoken');
+let models = require('../models');
+let ErrorCodes = require('../../error-codes').CODES;
+let ResponseJSON = require('../response');
 
 module.exports = function () {
     return function (req, res, next) {
-        var token = req.body.token || req.query.token || req.header['x-access-token'] || req.get('Authorization');
+        let token = req.body.token || req.query.token || req.header['x-access-token'] || req.get('Authorization');
         if (token) {
             jwt.verify(token, 'secretKey', function (err, decoded) {
                 if (err) {
-                    return res.status(401).send(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Authentication failed", "Authentication failed"));
+                    return res.status(401).send(ResponseJSON(ErrorCodes.ERROR_WRONG_PASSWORD, "Authentication failed", "Authentication failed"));
                 } else {
                     req.dbConnection = models('wi_' + decoded.username.toLowerCase(), (err) => {
                         console.log(err);
-                        if (err) return res.status(401).send(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Some err", "Some err"));
+                        if (err) return res.status(401).send(ResponseJSON(ErrorCodes.ERROR_WRONG_PASSWORD, "Some err", "Some err"));
                     });
-                    if (req.dbConnection) {
+                    req.dbConnection.sequelize.authenticate().then(() => {
                         req.decoded = decoded;
                         next();
-                    } else {
-                        return res.status(401).send(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "You are not activated", "You are not activated"));
-                    }
+                    }).catch(err => {
+                        return res.status(401).send(ResponseJSON(ErrorCodes.ERROR_WRONG_PASSWORD, "Error connecting to database", "Error connecting to database"));
+                    });
                 }
             });
 
