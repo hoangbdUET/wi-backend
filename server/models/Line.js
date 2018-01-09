e942989ad6cca05273af7f650ba0033ae3faac9a
Hoang Bui <bdhoang.ht@gmail.com>
2018-01-09 17:39:01 +0700

    add order numb to line

--- server/models/Line.js
+++ server/models/Line.js
@@ -113,6 +113,11 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING(100),
             allowNull: true,
             defaultValue: "N/A"
+        },
+        orderNum: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'a'
         }
     }, {
         paranoid: true
