4fff543a778a5c3e2c394835c1208d41ccba3bc4
hoang <bdhoang.ht@gmail.com>
2018-02-01 16:35:06 +0700

    fix family spec with null value

--- server/family/GlobalFamilyConditionUpdater.js
+++ server/family/GlobalFamilyConditionUpdater.js
@@ -71,7 +71,7 @@ function updateFamilySpec(workbook, sheetName, callback) {
                 eventEmitter.emit('fc-done');
             })
             .catch(function (err) {
-                // console.log("FAIL: FamilyCondition " + aFamilySpec.idFamilySpec + " Error:" + err, aFamilySpec.minScale);
+                console.log("FAIL: FamilyCondition " + aFamilySpec.idFamilySpec + " Error:" + err, aFamilySpec.minScale);
                 eventEmitter.emit('fc-done');
             });
 
@@ -95,8 +95,8 @@ function buildFamilySpec(row, sheet) {
     newComponent.idFamilySpec = parseInt(getValueAtCell(row, 0, sheet));
     newComponent.idFamily = parseInt(getValueAtCell(row, 1, sheet));
     newComponent.unit = getValueAtCell(row, 2, sheet) || null;
-    newComponent.minScale = parseFloat(getValueAtCell(row, 3, sheet)) === NaN ? null : parseFloat(getValueAtCell(row, 3, sheet));
-    newComponent.maxScale = parseFloat(getValueAtCell(row, 4, sheet)) === NaN ? null : parseFloat(getValueAtCell(row, 4, sheet));
+    newComponent.minScale = isNaN(parseFloat(getValueAtCell(row, 3, sheet))) ? null : parseFloat(getValueAtCell(row, 3, sheet));
+    newComponent.maxScale = isNaN(parseFloat(getValueAtCell(row, 4, sheet))) ? null : parseFloat(getValueAtCell(row, 4, sheet));
     newComponent.displayType = getValueAtCell(row, 5, sheet);
     newComponent.displayMode = getValueAtCell(row, 6, sheet);
     newComponent.blockPosition = getValueAtCell(row, 7, sheet);
