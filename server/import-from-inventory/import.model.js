57ed1d0353933d5dc0df857f3d44724ad3c5b3c6
hoang <bdhoang.ht@gmail.com>
2018-02-13 14:25:25 +0700

    update for batch

--- server/import-from-inventory/import.model.js
+++ server/import-from-inventory/import.model.js
@@ -54,75 +54,92 @@ async function importWell(well, token, callback, dbConnection, username) {
         let topDepth = _well.well_headers.find(h => h.header === 'STRT').value;
         let bottomDepth = _well.well_headers.find(h => h.header === 'STOP').value;
         let step = _well.well_headers.find(h => h.header === 'STEP').value;
-        let wiWell = await dbConnection.Well.create({
+        dbConnection.Well.create({
             name: _well.name,
             idProject: wiProject.idProject,
             topDepth: topDepth,
             bottomDepth: bottomDepth,
             step: step
-        });
-        asyncEach(_well.datasets, async function (dataset) {
-            let wiDataset = await dbConnection.Dataset.create({
-                name: dataset.name,
-                datasetKey: dataset.name,
-                datasetLabel: dataset.name,
-                idWell: wiWell.idWell
-            });
-            let queue = asyncQueue(function (curve, cb) {
-                let options = {
-                    method: 'POST',
-                    url: 'http://' + config.Service.inventory + '/user/well/dataset/curve/data',
-                    headers:
-                        {
-                            Authorization: token,
-                            'Content-Type': 'application/json'
-                        },
-                    body: {idCurve: curve.idCurve},
-                    json: true
-                };
-                dbConnection.Curve.create({
-                    name: curve.name,
-                    unit: curve.curve_revisions[0].unit,
-                    initValue: "batch",
-                    idDataset: wiDataset.idDataset
-                }).then(c => {
-                    let _curve = c;
-                    let curvePath = hashDir.createPath(config.curveBasePath, username + wiProject.name + wiWell.name + wiDataset.name + _curve.name, _curve.name + '.txt');
-                    try {
-                        let stream = request(options).pipe(fs.createWriteStream(curvePath));
-                        stream.on('close', function () {
-                            cb(null, _curve);
-                        });
-                        stream.on('error', function (err) {
+        }).then(wiWell => {
+            asyncEach(_well.datasets, async function (dataset) {
+                let wiDataset = await dbConnection.Dataset.create({
+                    name: dataset.name,
+                    datasetKey: dataset.name,
+                    datasetLabel: dataset.name,
+                    idWell: wiWell.idWell
+                });
+                let queue = asyncQueue(function (curve, cb) {
+                    let options = {
+                        method: 'POST',
+                        url: 'http://' + config.Service.inventory + '/user/well/dataset/curve/data',
+                        headers:
+                            {
+                                Authorization: token,
+                                'Content-Type': 'application/json'
+                            },
+                        body: {idCurve: curve.idCurve},
+                        json: true
+                    };
+                    dbConnection.Curve.create({
+                        name: curve.name,
+                        unit: curve.curve_revisions[0].unit,
+                        initValue: "batch",
+                        idDataset: wiDataset.idDataset
+                    }).then(c => {
+                        let _curve = c;
+                        let curvePath = hashDir.createPath(config.curveBasePath, username + wiProject.name + wiWell.name + wiDataset.name + _curve.name, _curve.name + '.txt');
+                        try {
+                            let stream = request(options).pipe(fs.createWriteStream(curvePath));
+                            stream.on('close', function () {
+                                cb(null, _curve);
+                            });
+                            stream.on('error', function (err) {
+                                cb(err, null);
+                            });
+                        } catch (err) {
                             cb(err, null);
-                        });
-                    } catch (err) {
-                        cb(err, null);
-                    }
+                        }
+                    });
+                }, 2);
+                queue.drain = function () {
+                    console.log("All Curve Done");
+                };
+                dataset.curves.forEach(function (curve) {
+                    queue.push(curve, function (err, success) {
+                    });
                 });
-            }, 2);
-            queue.drain = function () {
-                console.log("All Curve Done");
-            };
-            dataset.curves.forEach(function (curve) {
-                queue.push(curve, function (err, success) {
+            }, function () {
+                let wellHeaders = _well.well_headers;
+                asyncEach(wellHeaders, function (wellHeader, next) {
+                    dbConnection.WellHeader.findOrCreate({
+                        where: {header: wellHeader.header, idWell: wiWell.idWell}, defaults: {
+                            header: wellHeader.header,
+                            value: wellHeader.value,
+                            idWell: wiWell.idWell
+                        }
+                    })
                 });
+                callback(null, wiWell);
             });
-        }, function () {
-            let wellHeaders = _well.well_headers;
-            asyncEach(wellHeaders, function (wellHeader, next) {
-                dbConnection.WellHeader.findOrCreate({
-                    where: {header: wellHeader.header, idWell: wiWell.idWell}, defaults: {
-                        header: wellHeader.header,
-                        value: wellHeader.value,
-                        idWell: wiWell.idWell
+        }).catch(err => {
+            if (err.name === "SequelizeUniqueConstraintError") {
+                dbConnection.Well.findOne({
+                    where: {
+                        [Op.and]: [
+                            {name: {[Op.eq]: _well.name}},
+                            {idProject: wiProject.idProject}
+                        ]
                     }
-                })
-            });
-            callback(null, wiWell);
+                }).then(existedWell => {
+                    callback({idProject: wiProject.idProject, reason: "Well existed!", well_existed: existedWell}, null);
+                });
+            } else {
+                callback({idProject: wiProject.idProject, reason: "Error"}, null);
+            }
         });
     } catch (e) {
         if (e.name === "SequelizeUniqueConstraintError") {
+            console.log(e);
             callback({idProject: wiProject.idProject, reason: "Well existed!"}, null);
         } else {
             console.log(e);
