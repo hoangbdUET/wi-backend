8adf427b819e9b505d61e1d425c2517875ce4816
hoang <bdhoang.ht@gmail.com>
2018-07-19 16:51:20 +0700

    fix payload to large workflow

--- server/workflow/workflow.router.js
+++ server/workflow/workflow.router.js
@@ -6,9 +6,10 @@ let router = express.Router();
 let bodyParser = require('body-parser');
 let path = require('path');
 let fs = require('fs');
+let multer = require('multer');
+let upload = multer();
 
-router.use(bodyParser.json({limit: '50mb'}));
-router.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
+router.use(bodyParser.json());
 
 function writeToTmpFile(data, callback, type) {
     let time = Date.now();
@@ -23,18 +24,22 @@ router.post('/workflow/list', function (req, res) {
     }, req.dbConnection, req.decoded.username);
 });
 
-router.post('/workflow/new', function (req, res) {
+router.post('/workflow/new', upload.array(), function (req, res) {
     writeToTmpFile(req.body.content, function (tmpContentPath) {
         req.body.content = tmpContentPath;
+        req.body.createdBy = req.createdBy;
+        req.body.updatedBy = req.updatedBy;
         workflowModel.createWorkflow(req.body, function (done) {
             res.send(done);
         }, req.dbConnection, req.decoded.username);
     }, 'WORKFLOW_CONTENT');
 });
 
-router.post('/workflow/edit', function (req, res) {
+router.post('/workflow/edit', upload.array(), function (req, res) {
     writeToTmpFile(req.body.content, function (tmpContentPath) {
         req.body.content = tmpContentPath;
+        req.body.createdBy = req.createdBy;
+        req.body.updatedBy = req.updatedBy;
         workflowModel.editWorkflow(req.body, function (done) {
             res.send(done);
         }, req.dbConnection, req.decoded.username);
