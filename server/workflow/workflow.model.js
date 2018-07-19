4fc996579e2ede7acfe9f58a45ff9a5386fe1489
hoang <bdhoang.ht@gmail.com>
2018-07-19 16:12:25 +0700

    fix payload to large workflow

--- server/workflow/workflow.model.js
+++ server/workflow/workflow.model.js
@@ -3,13 +3,28 @@
 let ResponseJSON = require('../response');
 let ErrorCodes = require('../../error-codes').CODES;
 let async = require('async');
+let fs = require('fs-extra');
+let hashDir = require('wi-import').hashDir;
+let config = require('config');
 
-let createWorkflow = function (data, callback, dbConnection) {
+let createWorkflow = function (data, callback, dbConnection, username) {
+    let contentTmpPath = data.content;
+    data.content = '{}';
     if (!data.idWorkflowSpec) {
         callback(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "idWorkflowSpec can not be null", "idWorkflowSpec can not be null"));
     } else {
         dbConnection.Workflow.create(data).then(w => {
-            callback(ResponseJSON(ErrorCodes.SUCCESS, "Successful", w));
+            let savePath = hashDir.createPath(config.curveBasePath, 'WORKFLOW' + username + w.name, w.name + '.txt');
+            fs.copy(contentTmpPath, savePath, function (err) {
+                if (err) {
+                    fs.unlinkSync(contentTmpPath);
+                    console.log("Err copy workflow content ", err);
+                }
+                console.log("Copy workflow content file success! ", savePath);
+                fs.unlinkSync(contentTmpPath);
+                w.content = JSON.parse(fs.readFileSync(savePath).toString());
+                callback(ResponseJSON(ErrorCodes.SUCCESS, "Successful", w));
+            });
         }).catch(err => {
             if (err.name === "SequelizeUniqueConstraintError") {
                 callback(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Workflow name existed!"));
@@ -20,14 +35,27 @@ let createWorkflow = function (data, callback, dbConnection) {
     }
 };
 
-let editWorkflow = function (data, callback, dbConnection) {
+let editWorkflow = function (data, callback, dbConnection, username) {
     delete data.createdBy;
+    let contentTmpPath = data.content;
+    data.content = '{}';
     dbConnection.Workflow.findById(data.idWorkflow).then(w => {
         if (w) {
             Object.assign(w, data).save().then(rs => {
-                callback(ResponseJSON(ErrorCodes.SUCCESS, "Successful", rs));
+                let oldSavePath = hashDir.createPath(config.curveBasePath, 'WORKFLOW' + username + w.name, w.name + '.txt');
+                let newSavePath = hashDir.createPath(config.curveBasePath, 'WORKFLOW' + username + rs.name, rs.name + '.txt');
+                fs.copy(contentTmpPath, newSavePath, function (err) {
+                    if (err) {
+                        fs.unlinkSync(contentTmpPath);
+                        console.log("Err copy workflow content ", err);
+                    }
+                    if (oldSavePath !== newSavePath) fs.unlinkSync(oldSavePath);
+                    console.log("Copy workflow content file success! ", newSavePath);
+                    fs.unlinkSync(contentTmpPath);
+                    rs.content = JSON.parse(fs.readFileSync(newSavePath).toString());
+                    callback(ResponseJSON(ErrorCodes.SUCCESS, "Successful", rs));
+                });
             }).catch(err => {
-
                 if (err.name === "SequelizeUniqueConstraintError") {
                     callback(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Workflow name existed!"));
                 } else {
@@ -43,10 +71,12 @@ let editWorkflow = function (data, callback, dbConnection) {
 };
 
 
-let infoWorkflow = function (data, callback, dbConnection) {
+let infoWorkflow = function (data, callback, dbConnection, username) {
     dbConnection.Workflow.findById(data.idWorkflow).then(async w => {
         if (w) {
+            let savePath = hashDir.createPath(config.curveBasePath, 'WORKFLOW' + username + w.name, w.name + '.txt');
             w = w.toJSON();
+            w.content = JSON.parse(fs.readFileSync(savePath).toString());
             w.workflowspec = {};
             if (w.idWorkflowSpec) {
                 w.workflowspec = await dbConnection.WorkflowSpec.findById(w.idWorkflowSpec);
@@ -63,11 +93,13 @@ let infoWorkflow = function (data, callback, dbConnection) {
 };
 
 
-let deleteWorkflow = function (data, callback, dbConnection) {
+let deleteWorkflow = function (data, callback, dbConnection, username) {
     dbConnection.Workflow.findById(data.idWorkflow).then(w => {
         if (w) {
+            let savePath = hashDir.createPath(config.curveBasePath, 'WORKFLOW' + username + w.name, w.name + '.txt');
             w.setDataValue('updatedBy', data.updatedBy);
             w.destroy().then(() => {
+                fs.unlinkSync(savePath);
                 callback(ResponseJSON(ErrorCodes.SUCCESS, "Successful", w));
             });
         } else {
@@ -78,11 +110,13 @@ let deleteWorkflow = function (data, callback, dbConnection) {
     });
 };
 
-let listWorkflow = function (data, callback, dbConnection) {
+let listWorkflow = function (data, callback, dbConnection, username) {
     let res = [];
     dbConnection.Workflow.findAll({where: {idProject: data.idProject}}).then(w => {
         async.each(w, function (wf, next) {
             wf = wf.toJSON();
+            let savePath = hashDir.createPath(config.curveBasePath, 'WORKFLOW' + username + wf.name, wf.name + '.txt');
+            wf.content = JSON.parse(fs.readFileSync(savePath).toString());
             dbConnection.WorkflowSpec.findById(wf.idWorkflowSpec).then(ws => {
                 wf.workflowSpec = {
                     name: ws.name,
--- server/workflow/workflow.router.js
+++ server/workflow/workflow.router.js
@@ -4,39 +4,53 @@ let workflowModel = require('./workflow.model');
 let express = require('express');
 let router = express.Router();
 let bodyParser = require('body-parser');
+let path = require('path');
+let fs = require('fs');
+
 router.use(bodyParser.json({limit: '50mb'}));
-router.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+router.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
+
+function writeToTmpFile(data, callback, type) {
+    let time = Date.now();
+    let tmpPath = path.join(__dirname, type + '_' + time + '.txt');
+    fs.writeFileSync(tmpPath, JSON.stringify(data));
+    callback(tmpPath);
+}
 
 router.post('/workflow/list', function (req, res) {
     workflowModel.listWorkflow(req.body, function (done) {
         res.send(done);
-    }, req.dbConnection);
+    }, req.dbConnection, req.decoded.username);
 });
 
 router.post('/workflow/new', function (req, res) {
-    req.body.content = req.body.content || "{}";
-    workflowModel.createWorkflow(req.body, function (done) {
-        res.send(done);
-    }, req.dbConnection);
+    writeToTmpFile(req.body.content, function (tmpContentPath) {
+        req.body.content = tmpContentPath;
+        workflowModel.createWorkflow(req.body, function (done) {
+            res.send(done);
+        }, req.dbConnection, req.decoded.username);
+    }, 'WORKFLOW_CONTENT');
 });
 
 router.post('/workflow/edit', function (req, res) {
-    req.body.content = req.body.content || "{}";
-    workflowModel.editWorkflow(req.body, function (done) {
-        res.send(done);
-    }, req.dbConnection);
+    writeToTmpFile(req.body.content, function (tmpContentPath) {
+        req.body.content = tmpContentPath;
+        workflowModel.editWorkflow(req.body, function (done) {
+            res.send(done);
+        }, req.dbConnection, req.decoded.username);
+    }, 'WORKFLOW_CONTENT');
 });
 
 router.post('/workflow/info', function (req, res) {
     workflowModel.infoWorkflow(req.body, function (done) {
         res.send(done);
-    }, req.dbConnection);
+    }, req.dbConnection, req.decoded.username);
 });
 
 router.delete('/workflow/delete', function (req, res) {
     workflowModel.deleteWorkflow(req.body, function (done) {
         res.send(done);
-    }, req.dbConnection);
+    }, req.dbConnection, req.decoded.username);
 });
 
 module.exports = router;
