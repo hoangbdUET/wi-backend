85fab8be974ba6045776c2d4a4b0d4ea8e3e7f28
hoang <bdhoang.ht@gmail.com>
2018-08-30 17:46:46 +0700

    update

--- server/family-unit/family-unit.model.js
+++ server/family-unit/family-unit.model.js
@@ -5,12 +5,12 @@ let async = require('async');
 function getListUnitByIdFamily(idFamily, dbConnection) {
     return new Promise(function (resolve) {
         dbConnection.Family.findById(idFamily, {
+            order: ['name'],
             include: {
                 model: dbConnection.FamilySpec,
                 as: 'family_spec',
                 // where: {isDefault: true}
-            },
-            order: [['name', 'DESC']]
+            }
         }).then(family => {
             if (family.family_spec[0].idUnitGroup) {
                 dbConnection.FamilyUnit.findAll({where: {idUnitGroup: family.family_spec[0].idUnitGroup}}).then(units => {
@@ -47,7 +47,7 @@ let getListUnit = function (data, callback, dbConnection) {
         if (data.idUnitGroup) {
             dbConnection.FamilyUnit.findAll({
                 where: {idUnitGroup: data.idUnitGroup},
-                order: [['name', 'DESC']]
+                order: ['name']
             }).then(units => {
                 // response.sort((a, b) => {
                 //     let nameA = a.name.toUpperCase();
