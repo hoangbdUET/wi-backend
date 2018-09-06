2c89c5957180ca9556f8b5c1252603d1686fae39
hoang <bdhoang.ht@gmail.com>
2018-09-06 15:24:12 +0700

    fix cs

--- server/models/Annotation.js
+++ server/models/Annotation.js
@@ -25,7 +25,7 @@ module.exports = function (sequelize, DataTypes) {
             }
         },
         vAlign: {
-            type: DataTypes.ENUM('Top', 'Center', 'Bottom'),
+            type: DataTypes.ENUM('top', 'middle', 'bottom'),
             allowNull: false,
             defaultValue: 'Center'
         },
