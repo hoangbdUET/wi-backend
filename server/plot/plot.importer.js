2742c17a52c553a5172d45335da0ddb84391c202
BuiHoang <bdhoang.ht@gmail.com>
2018-12-03 14:43:04 +0700

    fix Mo

--- server/plot/plot.importer.js
+++ server/plot/plot.importer.js
@@ -6,23 +6,23 @@ let ErrorCodes = require('../../error-codes').CODES;
 let createdBy;
 let updatedBy;
 
-function findCurve(curve, dbConnection, idProject) {
+function findCurve(curve, dbConnection, idProject, well) {
     return new Promise((resolve => {
         if (!curve) return resolve(null);
-        dbConnection.Well.findOne({where: {name: curve.well, idProject: idProject}}).then(w => {
-            if (!w) resolve(null);
+        dbConnection.Well.findOne({where: {name: well.name, idProject: idProject}}).then(w => {
+            if (!w) return resolve(null);
             dbConnection.Dataset.findOne({where: {name: curve.dataset, idWell: w.idWell}}).then(d => {
-                if (!d) resolve(null);
+                if (!d) return resolve(null);
                 dbConnection.Curve.findOne({where: {name: curve.curve, idDataset: d.idDataset}}).then(c => {
                     if (!c) {
-                        resolve(null);
+                        return resolve(null);
                     } else {
-                        resolve(c);
+                        return resolve(c);
                     }
                 });
             });
         }).catch(err => {
-            resolve(null);
+            return resolve(null);
         });
     }));
 }
@@ -42,10 +42,10 @@ function findWell(well, dbConnection, idProject) {
     }));
 }
 
-function findZoneSet(zoneSet, dbConnection, idProject) {
+function findZoneSet(zoneSet, dbConnection, idProject, well) {
     return new Promise((resolve => {
         if (!zoneSet) return resolve(null);
-        dbConnection.Well.findOne({where: {name: zoneSet.well, idProject: idProject}}).then(w => {
+        dbConnection.Well.findOne({where: {name: well.name, idProject: idProject}}).then(w => {
             if (!w) {
                 resolve(null);
             } else {
@@ -63,10 +63,10 @@ function findZoneSet(zoneSet, dbConnection, idProject) {
     }));
 }
 
-function findMarkerSet(markerSet, dbConnection, idProject) {
+function findMarkerSet(markerSet, dbConnection, idProject, well) {
     return new Promise((resolve => {
         if (!markerSet) return resolve(null);
-        dbConnection.Well.findOne({where: {name: markerSet.well, idProject: idProject}}).then(w => {
+        dbConnection.Well.findOne({where: {name: well.name, idProject: idProject}}).then(w => {
             if (!w) {
                 resolve(null);
             } else {
@@ -104,22 +104,22 @@ function createPlot(plot, dbConnection, idProject) {
     return dbConnection.Plot.create(plot);
 }
 
-async function createDepthAxis(depth_axis, dbConnection, idProject, idPlot) {
+async function createDepthAxis(depth_axis, dbConnection, idProject, idPlot, well) {
     depth_axis.idPlot = idPlot;
     depth_axis.createdBy = createdBy;
     depth_axis.updatedBy = updatedBy;
-    let well = await findWell(depth_axis.well, dbConnection, idProject);
-    let curve = await findCurve(depth_axis.curve, dbConnection, idProject);
+    // let well = await findWell(depth_axis.well, dbConnection, idProject);
+    let curve = await findCurve(depth_axis.curve, dbConnection, idProject, well);
     depth_axis.idWell = well ? well.idWell : null;
     depth_axis.idCurve = curve ? curve.idCurve : null;
     return dbConnection.DepthAxis.create(depth_axis);
 }
 
-async function createZoneTrack(zone_track, dbConnection, idProject, idPlot) {
+async function createZoneTrack(zone_track, dbConnection, idProject, idPlot, well) {
     zone_track.idPlot = idPlot;
     zone_track.createdBy = createdBy;
     zone_track.updatedBy = updatedBy;
-    let zone_set = await findZoneSet(zone_track.zone_set, dbConnection, idProject);
+    let zone_set = await findZoneSet(zone_track.zone_set, dbConnection, idProject, well);
     zone_track.idZoneSet = zone_set ? zone_set.idZoneSet : null;
     return dbConnection.ZoneTrack.create(zone_track);
 }
@@ -147,13 +147,13 @@ function createImageTrack(image_track, dbConnection, idProject, idPlot) {
     })
 }
 
-function createTrack(track, dbConnection, idProject, idPlot, username) {
+function createTrack(track, dbConnection, idProject, idPlot, username, well) {
     return new Promise(async resolve => {
         track.idPlot = idPlot;
         track.createdBy = createdBy;
         track.updatedBy = updatedBy;
-        let zone_set = await findZoneSet(track.zone_set, dbConnection, idProject);
-        let marker_set = await findMarkerSet(track.marker_set, dbConnection, idProject);
+        let zone_set = await findZoneSet(track.zone_set, dbConnection, idProject, well);
+        let marker_set = await findMarkerSet(track.marker_set, dbConnection, idProject, well);
         track.idZoneSet = zone_set ? zone_set.idZoneSet : null;
         track.idMarkerSet = marker_set ? marker_set.idMarkerSet : null;
         dbConnection.Track.create(track).then(_track => {
@@ -173,7 +173,7 @@ function createTrack(track, dbConnection, idProject, idPlot, username) {
                         line.idTrack = _track.idTrack;
                         line.createdBy = _track.createdBy;
                         line.updatedBy = _track.updatedBy;
-                        findCurve(line.curve, dbConnection, idProject).then(curve => {
+                        findCurve(line.curve, dbConnection, idProject, well).then(curve => {
                             if (!curve) {
                                 resolve();
                             } else {
@@ -191,7 +191,7 @@ function createTrack(track, dbConnection, idProject, idPlot, username) {
                         shading.idTrack = _track.idTrack;
                         shading.createdBy = createdBy;
                         shading.updatedBy = updatedBy;
-                        findCurve(shading.controle_curve, dbConnection, idProject).then(async crtlCurve => {
+                        findCurve(shading.controle_curve, dbConnection, idProject, well).then(async crtlCurve => {
                             shading.idControlCurve = crtlCurve ? crtlCurve.idCurve : null;
                             let left_line = await findLine(shading.left_line, dbConnection, _track.idTrack);
                             let right_line = await findLine(shading.right_line, dbConnection, _track.idTrack);
@@ -230,33 +230,35 @@ module.exports = function (req, done, dbConnection, username) {
     fs.readFile(filePath, 'utf8', async function (err, data) {
         if (err) console.log(err);
         let myPlot = JSON.parse(data);
-        myPlot.name = req.body.plotName;
+        let well = await dbConnection.Well.findById(req.body.idWell);
+        if (!well) return done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "No well found by id"));
+        myPlot.name = req.body.plotName + "-" + well.name;
         createPlot(myPlot, dbConnection, idProject).then(pl => {
             async.series([
                 function (cb) {
                     async.each(myPlot.tracks, (track, nextTrack) => {
-                        createTrack(track, dbConnection, idProject, pl.idPlot, username).then(() => {
+                        createTrack(track, dbConnection, idProject, pl.idPlot, username, well).then(() => {
                             nextTrack();
                         });
                     }, cb);
                 },
                 function (cb) {
                     async.each(myPlot.depth_axes, (depth_axis, nextDepth) => {
-                        createDepthAxis(depth_axis, dbConnection, idProject, pl.idPlot).then(() => {
+                        createDepthAxis(depth_axis, dbConnection, idProject, pl.idPlot, well).then(() => {
                             nextDepth();
                         });
                     }, cb());
                 },
                 function (cb) {
                     async.each(myPlot.zone_tracks, (zone_track, nextZoneTrack) => {
-                        createZoneTrack(zone_track, dbConnection, idProject, pl.idPlot).then(() => {
+                        createZoneTrack(zone_track, dbConnection, idProject, pl.idPlot, well).then(() => {
                             nextZoneTrack();
                         });
                     }, cb)
                 },
                 function (cb) {
                     async.each(myPlot.image_tracks, (image_track, nextImageTrack) => {
-                        createImageTrack(image_track, dbConnection, idProject, pl.idPlot).then(() => {
+                        createImageTrack(image_track, dbConnection, idProject, pl.idPlot, well).then(() => {
                             nextImageTrack();
                         });
                     }, cb)
@@ -266,7 +268,7 @@ module.exports = function (req, done, dbConnection, username) {
             });
         }).catch(err => {
             if (err.name === "SequelizeUniqueConstraintError") {
-                done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Plot's name already exists!"));
+                done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Plot's name already exists! " + myPlot.name));
             } else {
                 done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, err.message, err.message));
             }
