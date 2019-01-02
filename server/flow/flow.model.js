600ec18bbd95bdd12f4cdaedc03e21a5ea0c609b
BuiHoang <bdhoang.ht@gmail.com>
2019-01-02 16:07:21 +0700

    add flow template

--- server/flow/flow.model.js
+++ server/flow/flow.model.js
@@ -40,6 +40,44 @@ let createNewFlow = function (flow, done, dbConnection) {
 		}).catch(err => {
 			done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Error", err));
 		});
+	} else if (flow.idParameterSet) {
+		dbConnection.ParameterSet.findById(flow.idParameterSet).then(pt => {
+			if (!pt) return done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "No template found"));
+			dbConnection.Flow.create({
+				name: flow.name,
+				content: pt.content.content,
+				createdBy: flow.createdBy,
+				updatedBy: flow.updatedBy,
+				idProject: flow.idProject
+			}).then(fl => {
+				async.each(pt.content.tasks, (task, next) => {
+					dbConnection.Task.create({
+						name: task.name,
+						content: task.content,
+						createdBy: flow.createdBy,
+						updatedBy: flow.updatedBy,
+						idFlow: fl.idFlow,
+						idTaskSpec: task.idTaskSpec
+					}).then(t => {
+						next();
+					}).catch(err => {
+						console.log(err);
+						next();
+					})
+				}, () => {
+					const validateFlow = require('../project/project.model').validationFlow;
+					validateFlow(fl.idFlow, dbConnection).then(() => {
+						done(ResponseJSON(ErrorCodes.SUCCESS, "Done", fl));
+					});
+				});
+			}).catch(err => {
+				if (err.name === "SequelizeUniqueConstraintError") {
+					done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Flow's name already exists!"));
+				} else {
+					done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, err.message, err.message));
+				}
+			});
+		});
 	} else {
 		dbConnection.Flow.create(flow).then(f => {
 			done(ResponseJSON(ErrorCodes.SUCCESS, "Successful", f));
@@ -76,7 +114,10 @@ let infoFlow = function (flow, done, dbConnection) {
 let listFlow = function (flow, done, dbConnection) {
 	if (flow.listTemplate) {
 		dbConnection.Flow.findAll({where: {idProject: null}}).then(rs => {
-			done(ResponseJSON(ErrorCodes.SUCCESS, "Successful", rs));
+			dbConnection.ParameterSet.findAll({where: {idProject: flow.idProject, type: "FT"}}).then(fts => {
+				rs.push(...fts);
+				done(ResponseJSON(ErrorCodes.SUCCESS, "Successful", rs));
+			});
 		})
 	} else {
 		dbConnection.Flow.findAll({where: {idProject: flow.idProject, isTemplate: false}}).then(fs => {
@@ -99,11 +140,80 @@ let deleteFlow = function (flow, done, dbConnection) {
 
 
 let duplicateFlow = function (flow, done, dbConnection) {
-	done(ResponseJSON(200, "Done"));
+	if (!flow.name) {
+		done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Need flow name"));
+	} else {
+		dbConnection.Flow.findById(flow.idFlow, {include: {model: dbConnection.Task}}).then(fl => {
+			if (fl) {
+				dbConnection.Flow.create({
+					name: flow.name,
+					content: fl.content,
+					createdBy: flow.createdBy,
+					updatedBy: flow.updatedBy,
+					idProject: fl.idProject
+				}).then(f => {
+					async.each(fl.tasks, (task, next) => {
+						dbConnection.Task.create({
+							name: task.name,
+							content: task.content,
+							createdBy: flow.createdBy,
+							updatedBy: flow.updatedBy,
+							idFlow: f.idFlow,
+							idTaskSpec: task.idTaskSpec
+						}).then(t => {
+							console.log("Done task ", t.name, "-", f.name);
+							next()
+						}).catch(err => {
+							console.log(err);
+							next();
+						});
+					}, () => {
+						const validateFlow = require('../project/project.model').validationFlow;
+						validateFlow(f.idFlow, dbConnection).then(() => {
+							done(ResponseJSON(ErrorCodes.SUCCESS, "Done", f));
+						});
+					});
+				}).catch(err => {
+					if (err.name === "SequelizeUniqueConstraintError") {
+						done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Flow's name already exists!"));
+					} else {
+						done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, err.message, err.message));
+					}
+				});
+			} else {
+				done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "No flow found"));
+			}
+		});
+	}
 };
 
 let saveAsTemplate = function (flow, done, dbConnection) {
-	done(ResponseJSON(200, "Done"));
+	if (!flow.name) return done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "Need template name"));
+	dbConnection.Flow.findById(flow.idFlow, {include: {model: dbConnection.Task}}).then(fl => {
+		fl = fl.toJSON();
+		if (fl) {
+			let saveObjContent = {
+				content: fl.content,
+				tasks: fl.tasks.map(t => {
+					t.content.inputData = [];
+					t.content.paramData = [];
+					return t;
+				})
+			};
+			dbConnection.ParameterSet.create({
+				name: flow.name,
+				idProject: fl.idProject,
+				createdBy: flow.createdBy,
+				updatedBy: flow.updatedBy,
+				content: saveObjContent,
+				type: "FT"
+			}).then(() => {
+				done(ResponseJSON(ErrorCodes.SUCCESS, "Done"));
+			});
+		} else {
+			done(ResponseJSON(ErrorCodes.ERROR_INVALID_PARAMS, "No workflow found"));
+		}
+	});
 };
 
 module.exports = {
