b6e6f2782f056d7368f16379ec9263b3bc5b6130
BuiHoang <bdhoang.ht@gmail.com>
2019-01-08 14:34:29 +0700

    add plot field

--- server/models/Plot.js
+++ server/models/Plot.js
@@ -1,56 +1,71 @@
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('plot', {
-        idPlot: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING(255),
-            allowNull: false,
-            unique: "name-idProject"
-        },
-        option: {
-            type: DataTypes.STRING(250),
-            allowNull: true,
-        },
-        duplicated: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 1
-        },
-        currentState: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            defaultValue: "{}"
-        },
-        cropDisplay: {
-            type: DataTypes.BOOLEAN,
-            allowNull: true,
-            defaultValue: false
-        },
-        printSetting: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        },
-        unit: {
-            type: DataTypes.STRING(30),
-            allowNull: true,
-            defaultValue: 'm'
-        },
-        depthRefSpec: {
-            type: DataTypes.STRING(50),
-            allowNull: true
-        },
-        createdBy: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        updatedBy: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        }
-    }, {
-        paranoid: true
-    });
+	return sequelize.define('plot', {
+		idPlot: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		name: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+			unique: "name-idProject"
+		},
+		option: {
+			type: DataTypes.STRING(250),
+			allowNull: true,
+		},
+		duplicated: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 1
+		},
+		currentState: {
+			type: DataTypes.STRING,
+			allowNull: true,
+			defaultValue: "{}"
+		},
+		cropDisplay: {
+			type: DataTypes.BOOLEAN,
+			allowNull: true,
+			defaultValue: false
+		},
+		printSetting: {
+			type: DataTypes.TEXT,
+			allowNull: true
+		},
+		unit: {
+			type: DataTypes.STRING(30),
+			allowNull: true,
+			defaultValue: 'm'
+		},
+		depthRefSpec: {
+			type: DataTypes.STRING(50),
+			allowNull: true
+		},
+		notShowPatterns: {
+			type: DataTypes.BOOLEAN,
+			allowNull: true,
+			defaultValue: 1
+		},
+		separationWidth: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+			defaultValue: '1'
+		},
+		separationUnit: {
+			type: DataTypes.STRING(10),
+			allowNull: true,
+			defaultValue: 'inch'
+		},
+		createdBy: {
+			type: DataTypes.STRING(50),
+			allowNull: false,
+		},
+		updatedBy: {
+			type: DataTypes.STRING(50),
+			allowNull: false
+		}
+	}, {
+		paranoid: true
+	});
 };
