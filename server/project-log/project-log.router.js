74da6c4a54312447a0eaca0f168ec5806018a282
BuiHoang <bdhoang.ht@gmail.com>
2019-01-14 14:49:21 +0700

    add note to parameter set

---
+++ server/project-log/project-log.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/log/new', (req, res) => {
+
+});
+
+router.post('/log/list', (req, res) => {
+
+});
+
+router.post('/log/delete', (req, res) => {
+
+});
+
+module.exports = router;
