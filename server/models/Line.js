2ba352e452bfff82dc781b6e0be7a000cccede10
BuiHoang <bdhoang.ht@gmail.com>
2019-02-20 10:47:22 +0700

    add textBoundary line

--- server/models/Line.js
+++ server/models/Line.js
@@ -144,6 +144,11 @@ module.exports = function (sequelize, DataTypes) {
 			allowNull: false,
 			defaultValue: "Top"
 		},
+		textDisplayBoundary: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: 1
+		},
 		createdBy: {
 			type: DataTypes.STRING(50),
 			allowNull: false,
