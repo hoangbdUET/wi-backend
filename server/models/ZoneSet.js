cd2af53963c5601c361461ea3d5460f5b954537c
BuiHoang <bdhoang.ht@gmail.com>
2019-04-08 10:02:38 +0700

    huhu

--- server/models/ZoneSet.js
+++ server/models/ZoneSet.js
@@ -25,7 +25,7 @@ module.exports = function (sequelize, DataTypes) {
 			type: DataTypes.BOOLEAN,
 			allowNull: false,
 			defaultValue: 0
-		}
+		},
 		createdBy: {
 			type: DataTypes.STRING(50),
 			allowNull: false,
