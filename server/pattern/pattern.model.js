bbb0f4dd1223e6ac791f0778eb5cea9ca67efac4
BuiHoang <bdhoang.ht@gmail.com>
2019-04-26 11:10:19 +0700

    update pattern

--- server/pattern/pattern.model.js
+++ server/pattern/pattern.model.js
@@ -10,7 +10,7 @@ let getListPattern = function (payload, done) {
         if (row[0] !== '') {
             response[row[0]] = {
                 full_name: row[1],
-                src: '/pattern/' + row[0] + '.png',
+                src: '/pattern/' + row[0] + '_.png',
                 typical: row[2]
             }
         }
