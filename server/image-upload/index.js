5046a4e334d48a225ab01e0acdb0e1bf258cbd57
BuiHoang <bdhoang.ht@gmail.com>
2019-05-10 11:31:14 +0700

    update image upload

--- server/image-upload/index.js
+++ server/image-upload/index.js
@@ -5,19 +5,50 @@ const path = require('path');
 
 const cors = require('cors');
 const fs = require('fs');
+const fsX = require('fs-extra');
 const formidable = require('formidable');
 let ResponseJSON = require('../response');
 let errorCodes = require('../../error-codes');
 let hashDir = require('../utils/data-tool').hashDir; // hashDir.createPath();
 let Finder = require('fs-finder');
 let asyncEach = require('async/each');
-
+let bodyParser = require('body-parser');
 let router = express.Router();
 // let saveDir = path.join(__dirname, '../..', config.imageBasePath);
 let saveDir = process.env.BACKEND_IMAGE_BASE_PATH || config.imageBasePath;
-
+const multer = require('multer');
 
 router.use(cors());
+router.use(bodyParser.json());
+
+let storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		// let idImage = req.body.idImage || 0;
+		// console.log(req.body);
+		let savePath = saveDir + "/" + req.decoded.username;
+		if (!fs.existsSync(savePath)) {
+			fs.mkdirSync(savePath);
+		}
+		// let fileHashDir = hashDir.createPath(savePath, idImage + '', '');
+		cb(null, savePath);
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + file.originalname);
+	}
+});
+
+let upload = multer({storage: storage});
+router.post('/image-upload', upload.single('file'), (req, res) => {
+	let savePath = saveDir + "/" + req.decoded.username;
+	let idImage = req.body.idImage || 0;
+	let fileHashDir = hashDir.createPath(savePath, idImage + '', idImage + req.file.originalname.substring(req.file.originalname.lastIndexOf('.')));
+	fsX.copy(req.file.path, fileHashDir, (err, succ) => {
+		if (!err) fs.unlinkSync(req.file.path);
+	});
+	let fileDir = fileHashDir.replace(/\\/g, '/').replace(saveDir, '');
+	res.send(ResponseJSON(200, "Done", fileDir));
+});
+// router.post('/image-upload', imageUpload);
 
 router.post('/image-list', function (req, res) {
 	let savePath = path.join(saveDir, req.decoded.username);
@@ -62,17 +93,17 @@ router.post('/image-delete', (req, res) => {
 	res.send(ResponseJSON(200, "Done", req.body.imageUrl));
 });
 
-router.post('/image-upload', imageUpload);
-
 
 function imageUpload(req, res) {
 	//console.log("DIR NAME : " + saveDir);
+	console.log(req.body);
+	let idImage = req.body.idImage || 0;
 	let savePath = saveDir + "/" + req.decoded.username;
 	if (!fs.existsSync(savePath)) {
 		fs.mkdirSync(savePath);
 	}
 
-	var form = new formidable.IncomingForm();
+	let form = new formidable.IncomingForm();
 	form.multiples = false;
 	form.uploadDir = process.env.BACKEND_IMAGE_BASE_PATH || config.imageBasePath;
 
@@ -84,7 +115,7 @@ function imageUpload(req, res) {
 
 	});
 	form.on('file', function (name, file) {
-		let fileHashDir = hashDir.createPath(savePath, file.path, file.name);
+		let fileHashDir = hashDir.createPath(savePath, idImage + '', idImage + file.name.substring(file.name.lastIndexOf('.')));
 		fs.rename(file.path, fileHashDir, function (err) {
 			if (err) {
 				console.log(err);
