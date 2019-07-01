77ce50bfd6e442db59e3b4abe24f42b8131587a6
BuiHoang <bdhoang.ht@gmail.com>
2019-07-01 11:20:44 +0700

    fix

--- server/models/Track.js
+++ server/models/Track.js
@@ -1,115 +1,125 @@
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('track', {
-        idTrack: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        orderNum: {
-            type: DataTypes.STRING(200),
-            allowNull: false,
-            defaultValue: 'zz'
-        },
-        showTitle: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: true
-        },
-        title: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-            defaultValue: "Track 1"
-        },
-        topJustification: {
-            type: DataTypes.ENUM('Left', 'Center', 'Right'),
-            allowNull: false,
-            defaultValue: 'Center'
-        },
-        bottomJustification: {
-            type: DataTypes.ENUM('Left', 'Center', 'Right'),
-            allowNull: false,
-            defaultValue: 'Center'
-        },
-        showLabels: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false
-        },
-        showValueGrid: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: true
-        },
-        majorTicks: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 1
-        },
-        minorTicks: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 5
-        },
-        showDepthGrid: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: true
-        },
-        width: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-            defaultValue: 2
-        },
-        widthUnit: {
-            type: DataTypes.STRING(20),
-            allowNull: true,
-            defaultValue: "inch"
-        },
-        color: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-            defaultValue: 'white'
-        },
-        showEndLabels: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: true
-        },
-        labelFormat: {
-            type: DataTypes.STRING(150),
-            allowNull: true
-        },
-        zoomFactor: {
-            type: DataTypes.DOUBLE,
-            allowNull: false,
-            defaultValue: 1.0
-        },
-        zoneAboveCurve: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: true
-        },
-        zoneOpacity: {
-            type: DataTypes.DOUBLE,
-            allowNull: false,
-            defaultValue: 0.6
-        },
-        trackOffset: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-            defaultValue: 0
-        },
-	      limitingSpec: {
-        	  type: DataTypes.TEXT,
-		        allowNull: true
-	      },
-        createdBy: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        updatedBy: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        }
-    });
+	return sequelize.define('track', {
+		idTrack: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		orderNum: {
+			type: DataTypes.STRING(200),
+			allowNull: false,
+			defaultValue: 'zz'
+		},
+		showTitle: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: true
+		},
+		title: {
+			type: DataTypes.STRING(100),
+			allowNull: false,
+			defaultValue: "Track 1"
+		},
+		topJustification: {
+			type: DataTypes.ENUM('Left', 'Center', 'Right'),
+			allowNull: false,
+			defaultValue: 'Center'
+		},
+		bottomJustification: {
+			type: DataTypes.ENUM('Left', 'Center', 'Right'),
+			allowNull: false,
+			defaultValue: 'Center'
+		},
+		showLabels: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		},
+		showValueGrid: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: true
+		},
+		majorTicks: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 1
+		},
+		minorTicks: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 5
+		},
+		showDepthGrid: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: true
+		},
+		width: {
+			type: DataTypes.FLOAT,
+			allowNull: false,
+			defaultValue: 2
+		},
+		widthUnit: {
+			type: DataTypes.STRING(20),
+			allowNull: true,
+			defaultValue: "inch"
+		},
+		color: {
+			type: DataTypes.STRING(50),
+			allowNull: false,
+			defaultValue: 'white'
+		},
+		showEndLabels: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: true
+		},
+		labelFormat: {
+			type: DataTypes.STRING(150),
+			allowNull: true
+		},
+		zoomFactor: {
+			type: DataTypes.DOUBLE,
+			allowNull: false,
+			defaultValue: 1.0
+		},
+		zoneAboveCurve: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: true
+		},
+		zoneOpacity: {
+			type: DataTypes.DOUBLE,
+			allowNull: false,
+			defaultValue: 0.6
+		},
+		trackOffset: {
+			type: DataTypes.FLOAT,
+			allowNull: false,
+			defaultValue: 0
+		},
+		limitingSpec: {
+			type: DataTypes.TEXT,
+			allowNull: true
+		},
+		showName: {
+			type: DataTypes.BOOLEAN,
+			allowNull: true,
+			defaultValue: true
+		},
+		showDepth: {
+			type: DataTypes.BOOLEAN,
+			allowNull: true,
+			defaultValue: true
+		},
+		createdBy: {
+			type: DataTypes.STRING(50),
+			allowNull: false,
+		},
+		updatedBy: {
+			type: DataTypes.STRING(50),
+			allowNull: false
+		}
+	});
 };
