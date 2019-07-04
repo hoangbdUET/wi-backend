58c5b768cd27909db541d56fe9da7a7b207d77e3
BuiHoang <bdhoang.ht@gmail.com>
2019-07-04 16:46:20 +0700

    add limitingSpec

--- server/models/ImageTrack.js
+++ server/models/ImageTrack.js
@@ -1,64 +1,68 @@
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('image_track', {
-        idImageTrack: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true
-        },
-        showTitle: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: true
-        },
-        title: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-            defaultValue: 'Image Track 1'
-        },
-        topJustification: {
-            type: DataTypes.ENUM('Left', 'Center', 'Right'),
-            allowNull: false,
-            defaultValue: 'Center'
-        },
-        orderNum: {
-            type: DataTypes.STRING(200),
-            allowNull: false,
-            defaultValue: 'zz'
-        },
-        background: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-            defaultValue: 'white'
-        },
-        width: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-            defaultValue: 1
-        },
-        widthUnit: {
-            type: DataTypes.STRING(20),
-            allowNull: true,
-            defaultValue: "inch"
-        },
-        zoomFactor: {
-            type: DataTypes.DOUBLE,
-            allowNull: false,
-            defaultValue: 1.0
-        },
-        trackOffset: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-            defaultValue: 0
-        },
-        createdBy: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        updatedBy: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        }
+	return sequelize.define('image_track', {
+		idImageTrack: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true
+		},
+		showTitle: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: true
+		},
+		title: {
+			type: DataTypes.STRING(100),
+			allowNull: false,
+			defaultValue: 'Image Track 1'
+		},
+		topJustification: {
+			type: DataTypes.ENUM('Left', 'Center', 'Right'),
+			allowNull: false,
+			defaultValue: 'Center'
+		},
+		orderNum: {
+			type: DataTypes.STRING(200),
+			allowNull: false,
+			defaultValue: 'zz'
+		},
+		background: {
+			type: DataTypes.STRING(50),
+			allowNull: false,
+			defaultValue: 'white'
+		},
+		width: {
+			type: DataTypes.FLOAT,
+			allowNull: false,
+			defaultValue: 1
+		},
+		widthUnit: {
+			type: DataTypes.STRING(20),
+			allowNull: true,
+			defaultValue: "inch"
+		},
+		zoomFactor: {
+			type: DataTypes.DOUBLE,
+			allowNull: false,
+			defaultValue: 1.0
+		},
+		trackOffset: {
+			type: DataTypes.FLOAT,
+			allowNull: false,
+			defaultValue: 0
+		},
+		limitingSpec: {
+			type: DataTypes.TEXT,
+			allowNull: true
+		},
+		createdBy: {
+			type: DataTypes.STRING(50),
+			allowNull: false,
+		},
+		updatedBy: {
+			type: DataTypes.STRING(50),
+			allowNull: false
+		}
 
-    });
+	});
 };
--- server/models/ZoneTrack.js
+++ server/models/ZoneTrack.js
@@ -1,68 +1,72 @@
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('zone_track', {
-        idZoneTrack: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true
-        },
-        showTitle: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: true
-        },
-        title: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-            defaultValue: 'Zone1'
-        },
-        topJustification: {
-            type: DataTypes.ENUM('Left', 'Center', 'Right'),
-            allowNull: false,
-            defaultValue: 'Center'
-        },
-        bottomJustification: {
-            type: DataTypes.ENUM('Left', 'Center', 'Right'),
-            allowNull: false,
-            defaultValue: 'Center'
-        },
-        orderNum: {
-            type: DataTypes.STRING(200),
-            allowNull: false,
-            defaultValue: 'zz'
-        },
-        color: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-            defaultValue: 'white'
-        },
-        width: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-            defaultValue: 1
-        },
-        widthUnit: {
-            type: DataTypes.STRING(20),
-            allowNull: true,
-            defaultValue: "inch"
-        },
-        zoomFactor: {
-            type: DataTypes.DOUBLE,
-            allowNull: false,
-            defaultValue: 1.0
-        },
-        trackOffset: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-            defaultValue: 0
-        },
-        createdBy: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        updatedBy: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        }
-    });
+	return sequelize.define('zone_track', {
+		idZoneTrack: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true
+		},
+		showTitle: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: true
+		},
+		title: {
+			type: DataTypes.STRING(100),
+			allowNull: false,
+			defaultValue: 'Zone1'
+		},
+		topJustification: {
+			type: DataTypes.ENUM('Left', 'Center', 'Right'),
+			allowNull: false,
+			defaultValue: 'Center'
+		},
+		bottomJustification: {
+			type: DataTypes.ENUM('Left', 'Center', 'Right'),
+			allowNull: false,
+			defaultValue: 'Center'
+		},
+		orderNum: {
+			type: DataTypes.STRING(200),
+			allowNull: false,
+			defaultValue: 'zz'
+		},
+		color: {
+			type: DataTypes.STRING(50),
+			allowNull: false,
+			defaultValue: 'white'
+		},
+		width: {
+			type: DataTypes.FLOAT,
+			allowNull: false,
+			defaultValue: 1
+		},
+		widthUnit: {
+			type: DataTypes.STRING(20),
+			allowNull: true,
+			defaultValue: "inch"
+		},
+		zoomFactor: {
+			type: DataTypes.DOUBLE,
+			allowNull: false,
+			defaultValue: 1.0
+		},
+		trackOffset: {
+			type: DataTypes.FLOAT,
+			allowNull: false,
+			defaultValue: 0
+		},
+		limitingSpec: {
+			type: DataTypes.TEXT,
+			allowNull: true
+		},
+		createdBy: {
+			type: DataTypes.STRING(50),
+			allowNull: false,
+		},
+		updatedBy: {
+			type: DataTypes.STRING(50),
+			allowNull: false
+		}
+	});
 };
