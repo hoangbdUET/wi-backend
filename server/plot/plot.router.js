83096241ed8389a771efcc81552814cfb77a5211
BuiHoang <bdhoang.ht@gmail.com>
2019-08-26 10:07:22 +0700

    add ml projects

--- server/plot/plot.router.js
+++ server/plot/plot.router.js
@@ -23,13 +23,6 @@ router.post('/plot/info', function (req, res) {
 	plotModel.getPlotInfo(req.body, function (status) {
 		res.send(status);
 	}, req.dbConnection);
-	// plotModel.getPlotInfo(req.body, req.dbConnection).then(status => {
-	// 	console.log(status);
-	// 	res.send(status);
-	// }).catch(err => {
-	// 	res.send(err);
-	// });
-
 });
 router.post('/plot/new', function (req, res) {
 	plotModel.createNewPlot(req.body, function (status) {
