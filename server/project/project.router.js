beed3ed37c04bda7f80d2d0cb7f50c6477136a15
Hoang Dang <bdhoang.ht@gmail.com>
2019-09-13 10:22:39 +0700

    add username to list project

--- server/project/project.router.js
+++ server/project/project.router.js
@@ -14,7 +14,7 @@ router.registerHooks = function (io) {
 router.post('/project/list', function (req, res) {
 	projectModel.getProjectList(req.body, function (status) {
 		res.send(status);
-	}, req.dbConnection, req.decoded.username, req.decoded.realUser, req.token, req.decoded.company, req.logger);
+	}, req.dbConnection, req.decoded.username, req.decoded.realUser, req.token, req.decoded.company, req.logger, parseInt(req.decoded.role));
 });
 
 router.post('/project/list-of-all-user', function (req, res) {
