6d6dd32124760906c6e74cbdc33df5abee7cf89a
Hoang Dang <bdhoang.ht@gmail.com>
2019-10-23 10:23:29 +0700

    fix

--- server/log-view/log-view.router.js
+++ server/log-view/log-view.router.js
@@ -7,11 +7,15 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 router.post('/view-by-user', (req, res) => {
-  logViewModel.viewByUserName(req.decoded.username, respData => res.send(respData), req.token, req.get('CurrentProject'));
+    logViewModel.viewByUserName(req.decoded.username, respData => res.send(respData), req.token, req.get('CurrentProject'));
 });
 
 router.post('/view-by-object', (req, res) => {
-  logViewModel.viewByObject(req.body, respData => res.send(respData), req.token, req.get('CurrentProject'));
+    logViewModel.viewByObject(req.body, respData => res.send(respData), req.token, req.get('CurrentProject'));
+});
+
+router.post('/put-log', (req, res) => {
+    logViewModel.putLog(req.body, data => res.send(data), req.decoded.username);
 });
 
 module.exports = router;
