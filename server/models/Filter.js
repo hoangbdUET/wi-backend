8f2fbb6c81184738d67f80689c95c152f238af70
Nam Phan <sockyone>
2019-11-28 16:30:56 +0700

    filter unique name

--- server/models/Filter.js
+++ server/models/Filter.js
@@ -7,7 +7,8 @@ module.exports = function (sequelize, DataTypes) {
         },
         name: {
             type: DataTypes.STRING(50),
-            allowNull: false
+            allowNull: false,
+            unique: true
         },
         content: {
             type: DataTypes.TEXT('medium'),
