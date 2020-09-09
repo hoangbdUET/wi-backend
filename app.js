564614a1125a3f98895b1bd10166200162c64ae2
Hoang Bui <bdhoang.ht@gmail.com>
2020-09-09 10:16:52 +0700

    fix

--- app.js
+++ app.js
@@ -152,8 +152,8 @@ function main() {
 	let mlProjectRouter = require('./server/ml-project/ml-project.router');
 	let queue = {};
 	let http = require('http').Server(app);
-	app.use(express.json({limit: '1mb'}));
-	app.use(express.urlencoded({limit: '1mb'}));
+	app.use(express.json({limit: '10mb'}));
+	app.use(express.urlencoded({limit: '10mb'}));
 	app.use(cors());
 	// app.use(queue({activeLimit: 2, queuedLimit: 2}));
 	/**
